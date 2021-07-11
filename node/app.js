34a9850c904a792cb57160861ef3f26e4f2bdd25
dstN <dstN@yinside.de>
2021-07-11 19:24:25 +0200

    get node-cron

--- node/app.js
+++ node/app.js
@@ -1,20 +1,42 @@
 require('dotenv')
   .config({ path: '../.env' });
 const Crawler = require('crawler');
-const { getDomainWithoutSuffix } = require('tldts');
 const mysql = require('mysql');
 const express = require("express");
-const fs = require("fs");
 const app = express();
-const results = require('./assets/result.json');
+const cron = require('node-cron');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.host,
   user: process.env.user,
   password: process.env.password,
   database: process.env.db,
   port: process.env.port
+});
+
+const c = new Crawler({
+  callback: (error, res, done) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const $ = res.$;
+
+      let rows = $("#main_table_countries_today tbody:first-of-type tr:not('.total_row_world')");
+    }
+    done();
+  }
+});
+
+app.get('/cron', function(req, res) {
+  const url = "https://www.worldometers.info/coronavirus/";
+  c.queue({
+    uri: url
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
